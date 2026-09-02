de685f6ff450b8dc5d7d8574e101cf40de52d556
samuelbolduc78 <e2469935@cmaisonneuve.qc.ca>
2026-09-02 11:00:15 -0400

    newnewphp

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+//inclure le ficihier commun contenant le haut du cide des ecrans des ui
+include_once('common/entete.inc.php');
+?>
+        <main class="page-accueil">
+            <article class="amorce">
+                <h1>Teeshirts, hoodies, et casquettes fabriqués au Québec 💯</h1>
+                <h2>Conçus au TIM avec ❤️</h2>
+                <h4>/** avec un p'tit peu d'aide de DALL-E 😉 **/</h4>
+            </article>
+            <article class="principal">
+                <p>
+                    Aujourd'hui, le teeshirt est un vêtement incontournable dans 
+                    le garde-robe de chacun. Et à l’aire où la question 
+                    environnementale se pose plus que jamais, on préfère les 
+                    teeshirts fabriqués au Québec afin de réduire l'impact 
+                    environnemental et mettre en avant l’artisanat local.
+                </p>
+                <p>
+                    Teeshirt en V, col rond, ample ou ajusté, à manches courtes 
+                    ou à manches longues pour les plus frileux, uni ou à motifs 
+                    pour styliser une tenue… Le teeshirt est disponible dans une 
+                    variété de couleurs, de styles et de motifs.
+                </p>
+            </article>
+        </main>
+<?php
+//inclure le ficihier p2p contenant le haut du cide des ecrans des ui
+include_once('common/p2p.inc.php');
+?>
---
+++ teeshirts.php
@@ -0,0 +1,16 @@
+<?php
+//inclure le ficihier commun contenant le haut du cide des ecrans des ui
+include_once('common/entete.inc.php');
+?>
+        <main class="page-teeshirts">
+            <article class="amorce">
+                <h1>Nos teeshirts</h1>
+            </article>
+            <article class="principal">
+                À venir...
+            </article>
+        </main>
+<?php
+//inclure le ficihier p2p contenant le haut du cide des ecrans des ui
+include_once('common/p2p.inc.php');
+?>
